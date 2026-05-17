04bbc170336c2e2e44ab4a0eb65d841e35d2c74c
루밀LuMir <rpfos@naver.com>
2026-05-17 22:56:58 +0900

    build(*): mark packages as side effect free

--- packages/react-kit/src/index.test.ts
+++ packages/react-kit/src/index.test.ts
@@ -7,7 +7,6 @@
 // --------------------------------------------------------------------------------
 
 import { assert, describe, it } from 'vitest';
-import { cn, frontmatter, frontmatterData } from './index.js';
 import packageJson from '../package.json' with { type: 'json' };
 
 // --------------------------------------------------------------------------------
@@ -18,23 +17,6 @@ describe('index', () => {
   describe('package.json', () => {
     it('should have `sideEffects: false`', () => {
       assert.strictEqual(packageJson.sideEffects, false);
-    });
-  });
-
-  describe('exports', () => {
-    it('`cn` should be defined', () => {
-      assert.isDefined(cn);
-      assert.strictEqual(typeof cn, 'function');
-    });
-
-    it('`frontmatter` should be defined', () => {
-      assert.isDefined(frontmatter);
-      assert.strictEqual(typeof frontmatter, 'function');
-    });
-
-    it('`frontmatterData` should be defined', () => {
-      assert.isDefined(frontmatterData);
-      assert.strictEqual(typeof frontmatterData, 'function');
     });
   });
 });
