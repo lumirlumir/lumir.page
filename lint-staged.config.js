3c325ac7d83a45427b53eb23d78da6d5511656e2
루밀LuMir <rpfos@naver.com>
2026-05-20 00:29:41 +0900

    chore(*): enable ESLint cache

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ export default {
     'prettier --write --ignore-unknown',
     'editorconfig-checker -config .editorconfig-checker.json',
   ],
-  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md}': 'eslint --fix',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md}': 'eslint --fix --cache --cache-strategy content',
   '*.css': 'stylelint --fix',
   '*.md': 'markdownlint --fix',
   '*.{h,c,cpp}': 'clang-format -i',
