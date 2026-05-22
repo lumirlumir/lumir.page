7810358c9e3e68fae9fdb9a2e4a9c80c6764625e
루밀LuMir <rpfos@naver.com>
2026-05-22 17:59:43 +0900

    wip

--- apps/api/src/api/chat.ts
+++ apps/api/src/api/chat.ts
@@ -3,14 +3,16 @@
  * @see https://vercel.com/docs/functions Vercel
  * @see https://aistudio.google.com Gemini
  * @see https://ai.google.dev/gemini-api/docs Gemini
- * @see https://ai.google.dev/api/generate-content Gemini
+ * @see https://ai.google.dev/gemini-api/docs/openai Gemini
  * @see https://developers.openai.com/api/docs/models/gpt-5-nano OpenAI
+ * @see https://developers.openai.com/api/reference/resources/chat/subresources/completions/methods/create OpenAI
  */
 
 // --------------------------------------------------------------------------------
 // Import
 // --------------------------------------------------------------------------------
 
+import OpenAI from 'openai';
 import { ALLOWED_ORIGINS } from '../core/constants.ts';
 import { createCORSHeaders } from '../core/utils.ts';
 
@@ -20,11 +22,16 @@ import { createCORSHeaders } from '../core/utils.ts';
 
 const ALLOWED_METHODS = 'POST, OPTIONS';
 const GEMINI_MODEL = 'gemini-3.1-flash-lite';
-const GEMINI_API_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent`;
+const GEMINI_API_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/openai/`;
 
 // --------------------------------------------------------------------------------
 // Export
 // --------------------------------------------------------------------------------
+
+/**
+ * Singleton instance of `OpenAI` client. Initialized lazily to avoid unnecessary overhead.
+ */
+let openai: OpenAI | null = null;
 
 /**
  * `/api/chat` API route handler.
@@ -55,47 +62,52 @@ export default {
 
     switch (request.method) {
       case 'POST': {
-        const text = await request.text();
-        const body = {
-          contents: [
-            {
-              role: 'user',
-              parts: [{ text }],
-            },
-          ],
-          generationConfig: {
-            maxOutputTokens: 512,
-            temperature: 0.7,
-            thinkingConfig: {
-              thinkingLevel: 'LOW',
-            },
-          },
-        };
-
-        const response = await fetch(GEMINI_API_ENDPOINT, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'x-goog-api-key': process.env.GEMINI_API_KEY,
-          },
-          body: JSON.stringify(body),
+        // Initialize OpenAI client lazily.
+        openai ??= new OpenAI({
+          apiKey: process.env.GEMINI_API_KEY,
+          baseURL: GEMINI_API_ENDPOINT,
         });
 
-        if (!response.ok) {
-          return new Response(null, {
-            status: response.status,
-            statusText: response.statusText,
+        const text = await request.text();
+
+        try {
+          const completion = await openai.chat.completions.create({
+            model: GEMINI_MODEL,
+            temperature: 1,
+            top_p: 0.7,
+            presence_penalty: 0,
+            max_completion_tokens: 2048,
+            messages: [
+              {
+                role: 'user',
+                content: [
+                  {
+                    text,
+                    type: 'text',
+                  },
+                ],
+              },
+            ],
+          });
+
+          return new Response(completion?.choices?.[0]?.message?.content, {
+            status: 200,
+            statusText: 'OK',
             headers: corsHeaders,
           });
+        } catch (error) {
+          if (error instanceof OpenAI.APIError) {
+            return new Response(error.message, {
+              status: error.status ?? 502,
+              headers: corsHeaders,
+            });
+          } else {
+            return new Response('Internal Server Error', {
+              status: 500,
+              headers: corsHeaders,
+            });
+          }
         }
-
-        const newText = await response.text();
-
-        return new Response(newText, {
-          status: 200,
-          statusText: 'OK',
-          headers: corsHeaders,
-        });
       }
 
       case 'OPTIONS': {
