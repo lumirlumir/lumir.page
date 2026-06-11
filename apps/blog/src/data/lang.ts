77ed4e0173b0cd2051c1749be5f08418ba6f99f1
루밀LuMir <rpfos@naver.com>
2026-06-11 22:10:06 +0900

    wip

---
+++ apps/blog/src/data/lang.ts
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Defines supported lang routing keys.
+ */
+
+// --------------------------------------------------------------------------------
+// Typedef
+// --------------------------------------------------------------------------------
+
+/**
+ * Represents the supported lang key used in the route segment.
+ */
+export type LangKey = (typeof langKeys)[number];
+
+/**
+ * Represents the props that include a `lang` key for localized routes.
+ */
+export interface PropsWithLang {
+  /**
+   * The `lang` key for the current route, used to determine the language of the content.
+   */
+  readonly lang: LangKey;
+}
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
+
+/**
+ * Supported lang keys for localized routes.
+ */
+export const langKeys = ['ko', 'en'] as const;
