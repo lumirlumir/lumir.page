ef1c75563bfdff2c16cdb3d2f52a59de312140f2
루밀LuMir <rpfos@naver.com>
2026-06-12 17:50:20 +0900

    wip

--- apps/blog/src/app/[lang]/page.tsx
+++ apps/blog/src/app/[lang]/page.tsx
@@ -7,14 +7,30 @@
 // --------------------------------------------------------------------------------
 
 import Article from '@/components/layouts/article';
+import type { LangKey } from '@/data/lang';
 import { getGithubUsers } from '@/utils/fetch';
+
+// --------------------------------------------------------------------------------
+// Helper
+// --------------------------------------------------------------------------------
+
+function getDictionary(name: string) {
+  return {
+    ko: `안녕하세요, ${name}의 블로그입니다.`,
+    en: `Hello, It's ${name}'s blog.`,
+  };
+}
 
 // --------------------------------------------------------------------------------
 // Default Export
 // --------------------------------------------------------------------------------
 
-export default async function Page() {
-  const { name } = await getGithubUsers();
+export default async function Page({ params }: PageProps<'/[lang]'>) {
+  const awaitedParams = await params;
+  const lang = awaitedParams.lang as LangKey;
 
-  return <Article>{`Hello, It's ${name}'s blog!`}</Article>;
+  const { name } = await getGithubUsers();
+  const dictionary = getDictionary(name);
+
+  return <Article>{dictionary[lang]}</Article>;
 }
