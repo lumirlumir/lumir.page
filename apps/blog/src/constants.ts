4b9017f303a1ecf3e187614143971dc1066b21ab
루밀LuMir <rpfos@naver.com>
2026-06-14 13:54:00 +0900

    feat(blog): enhance metadata for SEO

--- apps/blog/src/constants.ts
+++ apps/blog/src/constants.ts
@@ -3,8 +3,20 @@
  */
 
 // --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { type LangRecord } from '@/data/lang';
+
+// --------------------------------------------------------------------------------
 // Export
 // --------------------------------------------------------------------------------
+
+// Name
+export const AUTHOR = {
+  ko: '김용현 (lumir)',
+  en: 'Yong Hyeon Kim (lumir)',
+} as const satisfies LangRecord<string>;
 
 // Website
 export const WEBSITE_NAME = 'lumir.page';
@@ -14,6 +26,7 @@ export const WEBSITE_URL = `https://${WEBSITE_NAME}`;
 export const GITHUB_REPO_OWNER = 'lumirlumir';
 export const GITHUB_REPO_NAME = WEBSITE_NAME;
 export const GITHUB_REPO_FULL_NAME = `${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}`;
+export const GITHUB_AUTHOR_URL = `https://github.com/${GITHUB_REPO_OWNER}`;
 
 // Google
 export const GOOGLE_GA_ID = 'G-2G4YHTE048';
