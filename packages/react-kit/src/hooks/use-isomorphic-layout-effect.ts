0d0b0b04ae22482cea6ec6ca0dbbf4f20f539788
LuMir <rpfos@naver.com>
2026-07-03 19:00:40 +0900

    feat(@lumir/react-kit): add `use-isomorphic-layout-effect`

---
+++ packages/react-kit/src/hooks/use-isomorphic-layout-effect.ts
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview `useIsomorphicLayoutEffect` hook.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { useEffect, useLayoutEffect } from 'react';
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
+
+/**
+ * `useIsomorphicLayoutEffect` is a React hook that provides the behavior of `useLayoutEffect`
+ * without triggering warnings during server-side rendering.
+ * During SSR, there is no DOM to synchronously measure or mutate, so React warns about using `useLayoutEffect`.
+ *
+ * This hook runs synchronously after DOM updates but before paint, making it ideal for:
+ *
+ * - Measuring DOM elements after render.
+ * - Applying DOM changes before paint.
+ * - Preventing UI flashes or layout shifts.
+ * - Supporting both client and server environments safely.
+ *
+ * @param effect The effect function.
+ * @param deps An optional array of dependencies.
+ *
+ * @example
+ * ```tsx
+ * import { useIsomorphicLayoutEffect } from '@lumir/react-kit/hooks';
+ *
+ * function Component() {
+ *   useIsomorphicLayoutEffect(() => {
+ *     // Code to be executed during the layout phase on the client side.
+ *   }, [dep1, dep2, ...]);
+ * }
+ * ```
+ */
+export const useIsomorphicLayoutEffect =
+  typeof window === 'undefined' ? useEffect : useLayoutEffect;
