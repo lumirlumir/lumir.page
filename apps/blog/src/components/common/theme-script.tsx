c7e8e086b5619a14e73667dbeb38614430dbb646
LuMir <rpfos@naver.com>
2026-07-12 15:16:28 +0900

    wip

--- apps/blog/src/components/common/theme-script.tsx
+++ apps/blog/src/components/common/theme-script.tsx
@@ -34,17 +34,8 @@ export default function ThemeScript() {
     <script
       // eslint-disable-next-line react/no-danger -- Safe because the script is hardcoded and does not include any user input.
       dangerouslySetInnerHTML={{
-        __html: `
-function getTheme() {
-  const themeLocalStorage = localStorage.getItem('data-theme');
-
-  if(themeLocalStorage) return themeLocalStorage;
-
-  return matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
-};
-
-document.documentElement.setAttribute('data-theme', getTheme());
-`.trim(),
+        __html:
+          "document.documentElement.setAttribute('data-theme', localStorage.getItem('data-theme') ?? (matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'));",
       }}
     />
   );
