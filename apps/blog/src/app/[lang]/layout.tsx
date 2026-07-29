428474b55fc1f736dcdd547b6da70acfa4b89562
lumir <rpfos@naver.com>
2026-07-29 23:54:03 +0900

    wip

--- apps/blog/src/app/[lang]/layout.tsx
+++ apps/blog/src/app/[lang]/layout.tsx
@@ -98,7 +98,7 @@ export default async function RootLayout({
           <main>
             <article>{children}</article>
           </main>
-          {nav}
+          <aside>{nav}</aside>
 
           <Analytics />
           <SpeedInsights />
